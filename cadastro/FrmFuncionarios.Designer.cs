ae19476663b6a92a64f9d2c4e6109813d83a0871
diego.py <diwalker13@icloud.com>
2023-06-12 06:26:45 -0300

    Telas

---
+++ cadastro/FrmFuncionarios.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace DPDV.cadastro
+{
+    partial class FrmFuncionarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmFuncionarios));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.cbCargo = new System.Windows.Forms.ComboBox();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.image = new System.Windows.Forms.PictureBox();
+            this.txtCpf = new System.Windows.Forms.MaskedTextBox();
+            this.txtTel = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(489, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefone:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Endereço:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(453, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Cargo:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Enabled = false;
+            this.txtNome.Location = new System.Drawing.Point(69, 23);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(327, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Enabled = false;
+            this.txtEnd.Location = new System.Drawing.Point(82, 104);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(310, 20);
+            this.txtEnd.TabIndex = 8;
+            // 
+            // cbCargo
+            // 
+            this.cbCargo.Enabled = false;
+            this.cbCargo.FormattingEnabled = true;
+            this.cbCargo.Items.AddRange(new object[] {
+            "Gerente",
+            "Atendente",
+            "Estoquista",
+            ""});
+            this.cbCargo.Location = new System.Drawing.Point(497, 104);
+            this.cbCargo.Name = "cbCargo";
+            this.cbCargo.Size = new System.Drawing.Size(137, 21);
+            this.cbCargo.TabIndex = 9;
+            // 
+            // grid
+            // 
+            this.grid.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.grid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.grid.Location = new System.Drawing.Point(27, 226);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(886, 232);
+            this.grid.TabIndex = 12;
+            this.grid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_CellClick);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(27, 485);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(97, 26);
+            this.btnNovo.TabIndex = 13;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Enabled = false;
+            this.btnSalvar.Location = new System.Drawing.Point(130, 485);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(97, 26);
+            this.btnSalvar.TabIndex = 14;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Enabled = false;
+            this.btnEditar.Location = new System.Drawing.Point(233, 485);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(97, 26);
+            this.btnEditar.TabIndex = 15;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Enabled = false;
+            this.btnExcluir.Location = new System.Drawing.Point(336, 485);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(97, 26);
+            this.btnExcluir.TabIndex = 16;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(916, 485);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(97, 26);
+            this.btnSair.TabIndex = 18;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // image
+            // 
+            this.image.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.image.Enabled = false;
+            this.image.Image = global::DPDV.Properties.Resources.pngegg;
+            this.image.Location = new System.Drawing.Point(720, 9);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(193, 211);
+            this.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image.TabIndex = 11;
+            this.image.TabStop = false;
+            this.image.Click += new System.EventHandler(this.image_Click);
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Enabled = false;
+            this.txtCpf.Location = new System.Drawing.Point(525, 19);
+            this.txtCpf.Mask = "000.000.000-00";
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(109, 20);
+            this.txtCpf.TabIndex = 7;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Enabled = false;
+            this.txtTel.Location = new System.Drawing.Point(92, 183);
+            this.txtTel.Mask = "(99) 00000-0000";
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(114, 20);
+            this.txtTel.TabIndex = 10;
+            // 
+            // FrmFuncionarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 534);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.cbCargo);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmFuncionarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Funcionários";
+            this.Load += new System.EventHandler(this.FrmFuncionarios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.ComboBox cbCargo;
+        private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.MaskedTextBox txtCpf;
+        private System.Windows.Forms.MaskedTextBox txtTel;
+    }
+}
---
+++ cadastro/FrmFuncionarios.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace DPDV.cadastro
+{
+    public partial class FrmFuncionarios : Form
+    {
+        conexao con = new conexao();
+        string sql;
+        SQLiteCommand cmd;
+        string foto;
+
+
+        public FrmFuncionarios()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text.ToString().Trim() == "")
+            {
+                MessageBox.Show("Preencha o campo Nome", "Cadastro Funcionários", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNome.Text = "";
+                txtNome.Focus();
+                return;
+            }
+
+
+            if (txtCpf.Text == "   ,   ,   -" || txtCpf.Text.Length < 14)
+            {
+                MessageBox.Show("Preencha o campo CPF", "Cadastro Funcionários", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtCpf.Focus();
+                return;
+            }
+
+            con.AbrirConexao();
+            sql = "INSERT INTO funcionarios(nome, cpf, endereco, tel, cargo, foto, data) VALUES (@nome, @cpf, @endereco, @tel, @cargo, @foto, date('now'))";
+
+            cmd = new SQLiteCommand(sql, con.con);
+            cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+            cmd.Parameters.AddWithValue("@cpf", txtCpf.Text);
+            cmd.Parameters.AddWithValue("@endereco", txtEnd.Text);
+            cmd.Parameters.AddWithValue("@tel", txtTel.Text);
+            cmd.Parameters.AddWithValue("@cargo", cbCargo.Text);
+            cmd.Parameters.AddWithValue("@foto", img());
+            cmd.ExecuteNonQuery();
+            con.FecharConexao();
+
+            LimparFoto();
+            //Listar();
+
+            MessageBox.Show("Cadastro realizado com sucesso!", "Cadastro de Funcinários", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            btnNovo.Enabled = true;
+            btnSalvar.Enabled = false;
+            btnEditar.Enabled = false;
+            btnExcluir.Enabled = false;
+            Listar();
+            DesabilitarCampos();
+            LimparCampos();
+
+
+        }
+
+
+
+        private byte[] img()
+        {
+            byte[] imagem_byte = null;
+            if (string.IsNullOrEmpty(foto)) // Verifica se a variável foto está vazia ou nula
+            {
+                return new byte[0]; // Retorna um array vazio ao invés de null
+            }
+
+            FileStream fs = new FileStream(foto, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            imagem_byte = br.ReadBytes((int)fs.Length);
+            return imagem_byte;
+        }
+
+        private void LimparFoto()
+        {
+            if (image != null) // Verifica se o objeto image é válido
+            {
+                image.Image = Properties.Resources.pngegg;
+                foto = "img/pngegg.png";
+            }
+        }
+
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmFuncionarios_Load(object sender, EventArgs e)
+        {
+            Listar();
+
+        }
+
+        private void Listar()
+        {
+            con.AbrirConexao();
+            sql = "SELECT * FROM funcionarios ORDER BY nome asc";
+            cmd = new SQLiteCommand(sql, con.con);
+            SQLiteDataAdapter da = new SQLiteDataAdapter();
+            da.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            grid.DataSource = dt;
+            con.FecharConexao();
+            FormatarGD();
+
+        }
+
+        private void image_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Imagens(*.jpg; *.png) | *.jpg; *.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                foto = dialog.FileName.ToString();
+                Image originalImage = Image.FromFile(foto);
+
+                // Redimensionar a imagem para o tamanho do PictureBox
+                Image resizedImage = originalImage.GetThumbnailImage(image.Width, image.Height, null, IntPtr.Zero);
+
+                // Atribuir a imagem redimensionada ao PictureBox
+                image.Image = resizedImage;
+
+                // Dispose da imagem original (para liberar recursos)
+                originalImage.Dispose();
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            habilitarCampos();
+            txtNome.Focus();
+        }
+
+        private void habilitarCampos()
+        {
+            btnSalvar.Enabled = true;
+            txtNome.Enabled = true;
+            txtCpf.Enabled = true;
+            txtEnd.Enabled = true;
+            txtTel.Enabled = true;
+            image.Enabled = true;
+            cbCargo.Enabled = true;
+            btnNovo.Enabled = false;
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Text = "";
+            txtCpf.Text = "";
+            txtEnd.Text = "";
+            txtTel.Text = "";
+            cbCargo.Text = "";
+        }
+
+        private void DesabilitarCampos()
+        {
+            txtNome.Enabled = false;
+            txtCpf.Enabled = false;
+            txtEnd.Enabled = false;
+            txtTel.Enabled = false;
+            cbCargo.Enabled = false;
+        }
+
+        private void FormatarGD()
+        {
+            grid.Columns[0].HeaderText = "ID";
+            grid.Columns[1].HeaderText = "Colaborador";
+            grid.Columns[2].HeaderText = "CPF";
+            grid.Columns[3].HeaderText = "Endereço";
+            grid.Columns[4].HeaderText = "Telefone";
+            grid.Columns[5].HeaderText = "Imagem";
+            grid.Columns[6].HeaderText = "Data";
+            grid.Columns[7].HeaderText = "Cargo";
+
+            grid.Columns[0].Width = 50;
+            grid.Columns[6].Width = 50;
+            grid.Columns[0].Visible = false;
+            grid.Columns[7].Visible = false;
+        }
+        private void grid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > -1)
+            {
+                habilitarCampos();
+                btnEditar.Enabled = true;
+                btnExcluir.Enabled = true;
+                txtNome.Text = grid.CurrentRow.Cells[1].Value.ToString();
+                txtCpf.Text = grid.CurrentRow.Cells[2].Value.ToString();
+                txtEnd.Text = grid.CurrentRow.Cells[3].Value.ToString();
+                txtTel.Text = grid.CurrentRow.Cells[4].Value.ToString();
+                cbCargo.Text = grid.CurrentRow.Cells[7].Value.ToString();
+
+                if (grid.CurrentRow.Cells[5].Value != DBNull.Value)
+                {
+                    if (grid.CurrentRow.Cells[5].Value is byte[] imagem)
+                    {
+                        MemoryStream ms = new MemoryStream(imagem);
+                        image.Image = Image.FromStream(ms);
+                    }
+                    else
+                    {
+                        // Lógica para lidar com o valor não esperado da célula
+                    }
+                }
+                else
+                {
+                    image.Image = Properties.Resources.pngegg;
+                }
+
+
+
+            }
+
+
+        }
+    } }
